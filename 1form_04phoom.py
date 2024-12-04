d7f1b2f59874d24181df39003813847a4e59622f
phoomkitiphoom <frogggrrr@gmail.com>
2024-12-04 16:11:41 +0700

    Add files via upload

---
+++ 1form_04phoom.py
@@ -0,0 +1,8 @@
+
+print("Welcome")
+print("\n\nสวัสดี กรอกข้อมูลตนเองครับ ") 
+stu_number = input ("เลขที่ ")
+nickname = input("ชื่อเล่น ")
+print("\n\nยินดีต้อนรับ ", nickname)
+print("เลขที่ ", stu_number)
+print("\n\nโชคดีครับ" )
